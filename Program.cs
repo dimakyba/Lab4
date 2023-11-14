f3421a9a88860ab9746c8393ed8177c77de9286d
Dmytro Kyba <dkyba2006@gmail.com>
2023-11-14 15:29:15 +0200

    Init repo, set up env

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lab4
+{
+  class Program {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Hello, World!");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab4
+Variant 6
